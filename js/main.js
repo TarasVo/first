3b24baf9d12df6dc4331d74eebe7f7c4eb8bbc1b
Taras <voy@gmail.com>
2022-05-09 14:13:22 +0300

    lesson 1

--- js/main.js
+++ js/main.js
@@ -1,2 +1,19 @@
-let p1 = document.querySelector('.p-1');
-p1.style.color = 'red';
+let numberOfFilms = +prompt('скільки фільмів ви переглянули','');
+
+const personalMovieDB = {
+count : numberOfFilms,
+movies : {},
+actors : {},
+genres : [],
+privat : false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?',''),
+      b = prompt('На сколько оцените его?',''),
+      c = prompt('Один из последних просмотренных фильмов?',''),
+      d = prompt('На сколько оцените его?','');
+
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+console.log(personalMovieDB.movies)
