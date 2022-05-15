43693507d69524b83c947767ac4756bdf4f7a217
Taras <voy@gmail.com>
2022-05-15 12:12:07 +0300

    two lesson

--- js/main.js
+++ js/main.js
@@ -8,12 +8,26 @@ genres : [],
 privat : false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?',''),
-      b = prompt('На сколько оцените его?',''),
-      c = prompt('Один из последних просмотренных фильмов?',''),
-      d = prompt('На сколько оцените его?','');
+for(let i = 0; i < 2; i++){
+    const a = prompt('Один из последних просмотренных фильмов?',''),
+    b = prompt('На сколько оцените его?','');
+
+    if(a != '' && b != '' && a.length < 50 && a != null && b != null){
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    }else{
+        console.log('ERROR');
+        i--;
+    }
+}
+
+if(personalMovieDB.count <10){
+    console.log("Просмотрено довольно мало фильмов");
+} else if(personalMovieDB.count >= 10 && personalMovieDB.count < 30){
+    console.log("Вы классический зритель");
+}else{
+    console.log(' ви кіноман');
+}
 
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-console.log(personalMovieDB.movies)
+console.log(personalMovieDB);
